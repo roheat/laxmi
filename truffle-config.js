5509e79510ce7807031fd1d282805cfa0c9b682f
roheat <rohithsrivathsav@gmail.com>
2020-02-29 07:16:11 +0530

    updated config and initial contract

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,35 @@
-const path = require("path");
+require("dotenv").config();
+const mnemonic = process.env.MNEMONIC;
+const HDWalletProvider = require("truffle-hdwallet-provider");
+// Create your own key for Production environments (https://infura.io/)
+const INFURA_ID = process.env.INFURA_ID || "d6760e62b67f4937ba1ea2691046f06d";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://ropsten.infura.io/v3/${INFURA_ID}`
+        ),
+      gasPrice: 10000000000,
+      network_id: 3
+    },
+    kovan: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://kovan.infura.io/v3/${INFURA_ID}`
+        ),
+      gasPrice: 10000000000,
+      network_id: 42
     }
   }
 };
