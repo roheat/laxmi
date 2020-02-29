347ef70cbda8de05182749272da2fdbd4127564c
roheat <rohithsrivathsav@gmail.com>
2020-02-29 12:23:59 +0530

    added main contracts

---
+++ contracts/Pouch.sol
@@ -0,0 +1,240 @@
+pragma solidity >=0.5.0;
+pragma experimental ABIEncoderV2;
+
+import "./interfaces/TokenInterface.sol";
+import "./interfaces/cTokenInterface.sol";
+import "./interfaces/PTokenInterface.sol";
+import "./libraries/SafeMath.sol";
+import "./PouchStorage.sol";
+
+contract Pouch is PTokenInterface, PouchStorage {
+    constructor(address delegateAddress) public {
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256(
+                    "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+                ),
+                keccak256("Pouch"),
+                keccak256("1"),
+                42, // kovan chainId
+                delegateAddress
+            )
+        );
+        admin = delegateAddress;
+    }
+
+    using SafeMath for uint256;
+
+    function deposit(
+        address holder,
+        uint256 value,
+        uint256 nonce,
+        bytes32 r,
+        bytes32 s,
+        uint8 v
+    ) public returns (bool) {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(DEPOSIT_TYPEHASH, holder, value, nonce))
+            )
+        );
+
+        require(holder != address(0), "Pouch/invalid-address-0");
+        require(holder == ecrecover(digest, v, r, s), "Pouch/invalid-permit");
+        require(nonce == nonces[holder]++, "Pouch/invalid-nonce");
+
+        // ** Check for sufficient Funds **
+        uint256 userBalance = daiToken.balanceOf(holder);
+        require(userBalance >= value, "Insufficient Funds");
+
+        daiToken.transferFrom(holder, address(this), value); // **Transfer User's DAI**
+        balances[holder] += value;
+        totalSupply += value;
+        emit Transfer(address(0), holder, value); // **Mint PCH tokens for the User**
+        daiToken.approve(cDaiAddress, value);
+        cDai.mint(value); // **Mint cDai  **
+        return true;
+    }
+
+    // ** Withdraw DAI**
+    function withdraw(
+        address holder,
+        uint256 value,
+        uint256 nonce,
+        bytes32 r,
+        bytes32 s,
+        uint8 v
+    ) public returns (bool) {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(WITHDRAW_TYPEHASH, holder, value, nonce))
+            )
+        );
+
+        require(holder != address(0), "Pouch/invalid-address-0");
+        require(holder == ecrecover(digest, v, r, s), "Pouch/invalid-permit");
+        require(value <= balances[holder], "Insufficient Funds");
+        require(nonce == nonces[holder]++, "Pouch/invalid-nonce");
+
+        // ** Burn Pouch Token **
+        _transfer(holder, address(0), value);
+        totalSupply -= value;
+
+        // **  Redeem User's DAI from compound and transfer it to user.**
+        cDai.redeemUnderlying(value);
+        daiToken.transfer(holder, value);
+        return true;
+    }
+
+    function transact(
+        address holder,
+        address to,
+        uint256 value,
+        uint256 nonce,
+        bytes32 r,
+        bytes32 s,
+        uint8 v
+    ) public returns (bool) {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(
+                    abi.encode(TRANSACT_TYPEHASH, holder, to, value, nonce)
+                )
+            )
+        );
+
+        require(holder != address(0), "Pouch/invalid-address-0");
+        require(holder == ecrecover(digest, v, r, s), "Pouch/invalid-permit");
+        require(value <= balances[holder], "Insufficient Funds");
+        require(to != address(0));
+        require(nonce == nonces[holder]++, "Pouch/invalid-nonce");
+
+        // ** Transfer Funds **
+        _transfer(holder, to, value);
+
+        // ** Transfer Rewards,if Any. **
+        // if (value >= 1e18){
+        //     uint profitInDai =  _checkProfits().mul(getExchangeRate());
+        //     uint checkProfitInDai = profitInDai.div(1e18);
+        //     if(checkProfitInDai >= 1e11){
+        //         uint myReward =  _randomReward();
+        //         cDai.redeemUnderlying(myReward);
+        //         daiToken.transfer(msg.sender, myReward);
+
+        //     return true;
+        //     }
+        // }
+        return true;
+    }
+
+    /* For Testing Purposes Only*/
+
+    function transactTest(
+        address holder,
+        address to,
+        uint256 value,
+        uint256 nonce,
+        bytes32 r,
+        bytes32 s,
+        uint8 v
+    ) public returns (bool) {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(
+                    abi.encode(TRANSACT_TYPEHASH, holder, to, value, nonce)
+                )
+            )
+        );
+
+        require(holder != address(0), "Pouch/invalid-address-0");
+        require(holder == ecrecover(digest, v, r, s), "Pouch/invalid-permit");
+        require(value <= balances[holder], "Insufficient Funds");
+        require(to != address(0));
+        require(nonce == nonces[holder]++, "Pouch/invalid-nonce");
+
+        // ** Transfer Funds **
+        _transfer(holder, to, value);
+
+        // ** Transfer Rewards, 1 PCH token. **
+        _mint(holder, 1e18);
+        emit Reward(address(this), holder, 1e18);
+        rewards[holder] += 1e18;
+        // if (value >= 1e18) {
+        // uint256 profitInDai = checkProfits().mul(getExchangeRate());
+        // uint256 checkProfitInDai = profitInDai.div(1e18);
+        // if (checkProfitInDai >= 1e6) {
+        //     uint256 userRewarded = _randomReward();
+        //     cDai.redeemUnderlying(userRewarded);
+        //     daiToken.transfer(holder, userRewarded);
+        //     return true;
+        // }
+        // }
+        return true;
+    }
+
+    function checkProfits() public view returns (uint256) {
+        uint256 adjustedTotalSupply = (getMySupply()).mul(1e8);
+        uint256 ourContractBalance = cDai.balanceOf(admin);
+        uint256 cDaiExchangeRateDivided = (cDai.exchangeRateStored()).div(1e10);
+
+        uint256 currentPrice = adjustedTotalSupply.div(cDaiExchangeRateDivided);
+        return ourContractBalance.sub(currentPrice);
+    }
+
+    function spitProfits() public returns (bool) {
+        // uint256 profit = checkProfits();
+        // cDai.transfer(msg.sender, profit);
+        cDai.transfer(msg.sender, 100);
+        return true;
+    }
+    function getExchangeRate() public view returns (uint256) {
+        return cDai.exchangeRateStored();
+    }
+
+    function getMySupply() public view returns (uint256) {
+        TokenInterface pouch = TokenInterface(admin);
+        return pouch.totalSupply();
+    }
+
+    function checkContractBalance() public view returns (uint256) {
+        return cDai.balanceOf(admin);
+    }
+    // ** Internal Functions **
+
+    function _mint(address _to, uint256 _value)
+        internal
+        returns (bool success)
+    {
+        balances[_to] += _value;
+        totalSupply += _value;
+        emit Transfer(address(0), _to, _value);
+        return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value)
+        internal
+        returns (bool success)
+    {
+        require(balances[_from] >= _value);
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+    function _randomReward() internal view returns (uint256) {
+        uint256 randomnumber = uint256(
+            keccak256(abi.encodePacked(now, msg.sender, block.number))
+        ) %
+            2;
+        return randomnumber.mul(1e6);
+    }
+
+}
---
+++ contracts/PouchToken.sol
@@ -0,0 +1,71 @@
+pragma solidity >=0.5.0;
+pragma experimental ABIEncoderV2;
+
+import "./interfaces/TokenInterface.sol";
+import "./interfaces/cTokenInterface.sol";
+import "./interfaces/PTokenInterface.sol";
+import "./PouchStorage.sol";
+
+contract PouchToken is PTokenInterface, PouchStorage {
+    uint256 private constant MAX_UINT256 = uint256(-1);
+
+    // --- ERC20 Data ---
+    string public constant name = "Pouch Token";
+    string public constant version = "1";
+    uint8 public constant decimals = 18;
+    string public constant symbol = "PCH";
+
+    // ** ERC-20 Standard Functions **
+
+    function transfer(address _to, uint256 _value)
+        external
+        returns (bool success)
+    {
+        require(balances[msg.sender] >= _value);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value)
+        external
+        returns (bool success)
+    {
+        uint256 allowance = allowed[_from][msg.sender];
+        require(balances[_from] >= _value && allowance >= _value);
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        if (allowance < MAX_UINT256) {
+            allowed[_from][msg.sender] -= _value;
+        }
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) external view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value)
+        external
+        returns (bool success)
+    {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender)
+        external
+        view
+        returns (uint256 remaining)
+    {
+        return allowed[_owner][_spender];
+    }
+
+    function supplyOf() external view returns (uint256 balance) {
+        return totalSupply;
+    }
+
+}
